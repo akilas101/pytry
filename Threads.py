048609f410ceccc2e886fc3fb9c2373e1beb06e1
akilas101 <akilasdesalegn10@gmail.com>
2022-12-10 16:06:25 +0300

    threads

--- Threads.py
+++ Threads.py
@@ -1,4 +1,5 @@
 import time
+from threading import Thread
 # def wanabeme():
 #     x = input('say my name')
 #     return x
@@ -24,4 +25,17 @@ complex_calculation()
 # we can import time and use it to time our processes
 print(f'single thread total time: {time.time() -start}')
 
+thread1 = Thread(target = ask_user)
+thread2 = Thread(target = complex_calculation)
 
+start = time.time()
+
+
+thread1.start()
+thread2.start()
+
+
+thread1.join()
+thread2.join()
+
+print(f'Two thread total time = {time.time() - start}')
